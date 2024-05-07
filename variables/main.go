b8a8e8ba8869cf7c69921b150d2208d02215a38f
Pol Gubau Amores <gubaupol@gmail.com>
2024-05-07 19:34:40 +0200

    first commit

---
+++ variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "World"
+
+	fmt.Printf("Hello, %s!\n", name)
+
+
+	var anotherName string
+
+	var myBoolean bool
+
+	anotherName = "Gopher"
+	fmt.Printf("My name is %s and my boolean is %t\n", anotherName, myBoolean)
+}
