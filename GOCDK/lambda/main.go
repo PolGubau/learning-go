b917e022a2b643e7cb17af238919998c470a093d
Pol Gubau Amores <gubaupol@gmail.com>
2024-05-07 23:24:39 +0200

    chore: Remove unnecessary code files

---
+++ GOCDK/lambda/main.go
@@ -0,0 +1,25 @@
+package lambda
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+// take payload
+func HandleRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "",fmt.Errorf("username is empty")
+	}
+	return fmt.Sprintf("Successfully processed - %s", event.Username),nil
+
+}
+
+// our backend
+func main() {
+	lambda.Start(HandleRequest)
+}
